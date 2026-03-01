bd2193de2369387ea9ef15455f29dc05a3200986
zenyasan <burioishi4560@gmail.com>
2026-03-01 09:05:09 +0900

    Update app.py

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ st.markdown("""
     }
     
     .main .block-container {
-        padding-top: 100rem;
+        padding-top: 28rem;
         padding-bottom: 2rem;
         max-width: 1400px;
     }
